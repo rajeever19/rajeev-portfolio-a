363380f7f3d0b554941899e78ec79425bcd9d34c
Rajeev Ranjan Singh <rajeevranjan54545@gmail.com>
2025-01-08 10:20:29 +0530

    hello

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/favicon.ico',
+        destination: '/some-other-path',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
